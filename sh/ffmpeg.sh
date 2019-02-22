5b285a1c3b65e5472455faf01f1cf1b03fa677f5
泸松松 <ms66@users.noreply.github.com>
2019-02-23 01:45:18 +0800

    Update ffmpeg.sh

--- sh/ffmpeg.sh
+++ sh/ffmpeg.sh
@@ -70,7 +70,7 @@ Install_ffmpeg()
 	wget http://ffmpeg.org/releases/ffmpeg-4.1.tar.bz2
 	tar -xjvf ffmpeg-4.1.tar.bz2
 	cd ffmpeg-4.1
-	./configure --enable-libmp3lame --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-version3 --enable-shared
+	./configure
 	make
 	make install
 	cd
