cdc7a957480a0aab4e81972713494eb3a1acc9d4
泸松松 <ms66@users.noreply.github.com>
2019-02-23 02:12:26 +0800

    Update ffmpeg.sh

--- sh/ffmpeg.sh
+++ sh/ffmpeg.sh
@@ -1,78 +1,18 @@
-Install_yasm()
-{
-	cd
-	wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
-	tar -xzvf yasm-1.3.0.tar.gz
-	cd yasm-1.3.0
-	./configure
-	make
-	make install
-	cd
-	rm -rf yasm*
-}
-Install_lame()
-{
-	cd
-	wget http://jaist.dl.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
-	tar -xzvf lame-3.99.5.tar.gz
-	cd lame-3.99.5
-	./configure
-	make
-	make install
-	cd
-	rm -rf lame*
-}
-Install_amr()
-{
-	cd
-	wget http://downloads.sourceforge.net/project/opencore-amr/opencore-amr/opencore-amr-0.1.3.tar.gz
-	tar -xzvf opencore-amr-0.1.3.tar.gz
-	cd opencore-amr-0.1.3
-	./configure
-	make
-	make install
-	cd
-	rm -rf opencore-amr*
-}
-Install_amrnb()
-{
-	cd
-	wget http://www.penguin.cz/~utx/ftp/amr/amrnb-11.0.0.0.tar.bz2
-	tar -xjvf amrnb-11.0.0.0.tar.bz2
-	cd amrnb-11.0.0.0
-	./configure
-	make
-	make install
-	cd
-	rm -rf amrnb*
-}
-Install_amrwb()
-{
-	cd
-	wget http://www.penguin.cz/~utx/ftp/amr/amrwb-11.0.0.0.tar.bz2
-	tar -xjvf amrwb-11.0.0.0.tar.bz2
-	cd amrwb-11.0.0.0
-	./configure
-	make
-	make install
-	cd
-	rm -rf amrwb*
-}
-Install_ffmpeg()
-{
-	cd
-	wget http://ffmpeg.org/releases/ffmpeg-4.1.tar.bz2
-	tar -xjvf ffmpeg-4.1.tar.bz2
-	cd ffmpeg-4.1
-	./configure
-	make
-	make install
-	cd
-	rm -rf ffmpeg*
-}
-Install_yasm
-Install_lame
-Install_amr
-Install_amrnb
-Install_amrwb
-Install_ffmpeg
+cd
+wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
+tar -xzvf yasm-1.3.0.tar.gz
+cd yasm-1.3.0
+./configure
+make
+make install
+cd
+rm -rf yasm*
+cd
+wget http://ffmpeg.org/releases/ffmpeg-4.1.tar.bz2
+tar -xjvf ffmpeg-4.1.tar.bz2
+cd ffmpeg-4.1
+./configure
+make
+make install
+cd
+rm -rf ffmpeg*
