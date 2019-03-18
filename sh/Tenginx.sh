1b773baf418657e6f46752eb3325cfc6d9815ff5
泸松松 <ms66@users.noreply.github.com>
2019-03-18 15:02:41 +0800

    Update Tenginx.sh

--- sh/Tenginx.sh
+++ sh/Tenginx.sh
@@ -1,7 +1,7 @@
 yum install gcc openssl-devel pcre-devel zlib-devel
 wget http://tengine.taobao.org/download/tengine-2.2.3.tar.gz
 tar -zxvf tengine-2.2.3.tar.gz
-cd tengine-2.2.3.tar.gz
+cd tengine-2.2.3
 ./configure
 make
 sudo make install
